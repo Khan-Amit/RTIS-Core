4a39c42af4f35736050313037d42cb27f0293251
ENKI <amit.khanna.1082@gmail.com>
2026-03-14 08:02:45 +0700

    Add HS_DATABASE with product details and categories

---
+++ hs_database.js
@@ -0,0 +1,63 @@
+const HS_DATABASE = [
+
+{
+product: "tshirt",
+category: "Garments",
+hs: "610910"
+},
+
+{
+product: "jeans",
+category: "Garments",
+hs: "620342"
+},
+
+{
+product: "face cream",
+category: "Cosmetics",
+hs: "330499"
+},
+
+{
+product: "hair color",
+category: "Cosmetics",
+hs: "330590"
+},
+
+{
+product: "plastic kitchenware",
+category: "Household",
+hs: "392410"
+},
+
+{
+product: "ceramic plate",
+category: "Household",
+hs: "691200"
+},
+
+{
+product: "electric kettle",
+category: "Electronics",
+hs: "851610"
+},
+
+{
+product: "led light",
+category: "Electronics",
+hs: "940540"
+},
+
+{
+product: "rice",
+category: "Food",
+hs: "100630"
+},
+
+{
+product: "green tea",
+category: "Food",
+hs: "090210"
+}
+
+];
