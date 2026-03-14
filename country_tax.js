4ac704334716cc728cc628cb7796526356b26f09
ENKI <amit.khanna.1082@gmail.com>
2026-03-14 08:06:29 +0700

    Add getCountryTax function for tax rates by country

---
+++ country_tax.js
@@ -0,0 +1,29 @@
+function getCountryTax(country){
+
+if(country === "bangladesh"){
+
+return 35;
+
+}
+
+if(country === "thailand"){
+
+return 30;
+
+}
+
+if(country === "china"){
+
+return 20;
+
+}
+
+if(country === "russia"){
+
+return 25;
+
+}
+
+return 20;
+
+}
