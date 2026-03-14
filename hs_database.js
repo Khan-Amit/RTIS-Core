1af99c2e3fe78de508fb5308ba97f9c9a6b9678a
ENKI <amit.khanna.1082@gmail.com>
2026-03-14 10:41:19 +0700

    Update hs_database.js

--- hs_database.js
+++ hs_database.js
@@ -1,31 +1,33 @@
 const hsDatabase = {
 
-"T-Shirt": {hs:"6109", marketLow:50000000, marketHigh:200000000},
-"Jeans": {hs:"6203", marketLow:40000000, marketHigh:150000000},
-"Jacket": {hs:"6201", marketLow:30000000, marketHigh:120000000},
-"Cotton Shirt": {hs:"6205", marketLow:35000000, marketHigh:130000000},
-"Sweater": {hs:"6110", marketLow:20000000, marketHigh:90000000},
-"Men Suit": {hs:"6103", marketLow:15000000, marketHigh:70000000},
-"Women Suit": {hs:"6104", marketLow:15000000, marketHigh:60000000},
-"Underwear": {hs:"6107", marketLow:60000000, marketHigh:250000000},
+"Cotton T-Shirt":{
+hs:"6109",
+marketLow:5000000,
+marketHigh:12000000
+},
 
-"Smartphone": {hs:"8517", marketLow:200000000, marketHigh:800000000},
-"Laptop": {hs:"8471", marketLow:150000000, marketHigh:600000000},
-"Tablet": {hs:"8471", marketLow:80000000, marketHigh:350000000},
-"Headphones": {hs:"8518", marketLow:50000000, marketHigh:250000000},
-"Microphone": {hs:"8518", marketLow:20000000, marketHigh:120000000},
-"Camera": {hs:"8525", marketLow:30000000, marketHigh:200000000},
-"Television": {hs:"8528", marketLow:100000000, marketHigh:400000000},
-"Router": {hs:"8517", marketLow:20000000, marketHigh:150000000},
+"Men Jacket":{
+hs:"6203",
+marketLow:4000000,
+marketHigh:9000000
+},
 
-"Rice": {hs:"1006", marketLow:500000000, marketHigh:1500000000},
-"Sugar": {hs:"1701", marketLow:300000000, marketHigh:900000000},
-"Coffee": {hs:"0901", marketLow:250000000, marketHigh:800000000},
-"Tea": {hs:"0902", marketLow:200000000, marketHigh:700000000},
+"Smartphone":{
+hs:"8517",
+marketLow:20000000,
+marketHigh:50000000
+},
 
-"Lipstick": {hs:"3304", marketLow:30000000, marketHigh:200000000},
-"Perfume": {hs:"3303", marketLow:50000000, marketHigh:250000000},
-"Face Cream": {hs:"3304", marketLow:60000000, marketHigh:300000000},
-"Shampoo": {hs:"3305", marketLow:80000000, marketHigh:350000000}
+"Laptop":{
+hs:"8471",
+marketLow:15000000,
+marketHigh:40000000
+},
+
+"Rice":{
+hs:"1006",
+marketLow:10000000,
+marketHigh:30000000
+}
 
 };
