209094d95db4490ac797c2338ad71ecd1f111b06
ENKI <amit.khanna.1082@gmail.com>
2026-03-14 11:13:05 +0700

    Update hs_database.js

--- hs_database.js
+++ hs_database.js
@@ -1,45 +1,25 @@
 const hsDatabase = {
 
-"Garment T-Shirt":{
-hs:"6109",
-marketLow:5000000,
-marketHigh:12000000
+Garments:{
+"T-Shirt":{hs:"6109",marketLow:5000000,marketHigh:12000000},
+"Jeans":{hs:"6203",marketLow:7000000,marketHigh:15000000},
+"Jacket":{hs:"6203",marketLow:4000000,marketHigh:9000000}
 },
 
-"Garment Polo Shirt":{
-hs:"6105",
-marketLow:4000000,
-marketHigh:10000000
+Cosmetics:{
+"Lipstick":{hs:"3304",marketLow:8000000,marketHigh:20000000},
+"Perfume":{hs:"3303",marketLow:12000000,marketHigh:30000000},
+"Face Cream":{hs:"3304",marketLow:10000000,marketHigh:25000000}
 },
 
-"Garment Men Jacket":{
-hs:"6203",
-marketLow:4000000,
-marketHigh:9000000
+Kitchenware:{
+"Cooking Pan":{hs:"7323",marketLow:6000000,marketHigh:18000000},
+"Kitchen Knife":{hs:"8211",marketLow:4000000,marketHigh:12000000}
 },
 
-"Garment Jeans":{
-hs:"6203",
-marketLow:7000000,
-marketHigh:15000000
-},
-
-"Smartphone":{
-hs:"8517",
-marketLow:20000000,
-marketHigh:50000000
-},
-
-"Laptop":{
-hs:"8471",
-marketLow:15000000,
-marketHigh:40000000
-},
-
-"Rice":{
-hs:"1006",
-marketLow:10000000,
-marketHigh:30000000
+Household:{
+"Vacuum Cleaner":{hs:"8508",marketLow:10000000,marketHigh:25000000},
+"LED Lamp":{hs:"9405",marketLow:7000000,marketHigh:22000000}
 }
 
 };
