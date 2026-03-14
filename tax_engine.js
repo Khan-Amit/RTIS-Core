cb5a6174f361ce2ae4fc8402ed820c9169c59e4c
ENKI <amit.khanna.1082@gmail.com>
2026-03-14 10:25:28 +0700

    Update tax_engine.js

--- tax_engine.js
+++ tax_engine.js
@@ -1,17 +1,69 @@
-function getCountryTax(country){
+function getCountryTax(country, hs){
 
-let taxRates = {
+const tariffs = {
 
-bangladesh:25,
-thailand:20,
-china:18,
-india:22,
-usa:10,
-eu:12,
-russia:15
+bangladesh:{
+6109:25,
+6203:30,
+8517:10,
+8471:5,
+1006:15
+},
+
+thailand:{
+6109:20,
+6203:25,
+8517:7,
+8471:0,
+1006:10
+},
+
+china:{
+6109:18,
+6203:20,
+8517:8,
+8471:3,
+1006:12
+},
+
+india:{
+6109:22,
+6203:28,
+8517:12,
+8471:8,
+1006:20
+},
+
+usa:{
+6109:16,
+6203:18,
+8517:0,
+8471:0,
+1006:5
+},
+
+eu:{
+6109:12,
+6203:15,
+8517:0,
+8471:0,
+1006:8
+},
+
+russia:{
+6109:15,
+6203:17,
+8517:5,
+8471:2,
+1006:10
+}
 
 };
 
-return taxRates[country] || 0;
+if(tariffs[country] && tariffs[country][hs]){
+return tariffs[country][hs];
+}
+
+return 10;
 
 }
