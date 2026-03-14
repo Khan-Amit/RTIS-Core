882d890ffe81b2ec6937fe7699dcce3dc13c730c
ENKI <amit.khanna.1082@gmail.com>
2026-03-14 09:58:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function loadProducts(){
 let category = document.getElementById("category").value;
 let productSelect = document.getElementById("product");
 
-productSelect.innerHTML = "<option>Select Product</option>";
+productSelect.innerHTML = "<option value=''>Select Product</option>";
 
 if(!categories[category]) return;
 
-categories[category].forEach(function(p){
+categories[category].forEach(function(product){
 
 let option = document.createElement("option");
-option.value = p;
-option.text = p;
+option.value = product;
+option.text = product;
 
 productSelect.appendChild(option);
 
@@ -20,14 +20,12 @@ productSelect.appendChild(option);
 }
 
 
+
 function showHS(){
 
 let product = document.getElementById("product").value;
 
-if(!hsDatabase[product]){
-console.log("Product not found:",product);
-return;
-}
+if(!hsDatabase[product]) return;
 
 document.getElementById("hs").value =
 hsDatabase[product].hs;
@@ -37,5 +35,53 @@ hsDatabase[product].marketLow;
 
 document.getElementById("marketHigh").value =
 hsDatabase[product].marketHigh;
+
+}
+
+
+
+function calculate(){
+
+let price = Number(document.getElementById("price").value);
+let qty = Number(document.getElementById("qty").value);
+let freight = Number(document.getElementById("freight").value);
+let insurance = Number(document.getElementById("insurance").value);
+
+let sell = Number(document.getElementById("sell").value);
+let sales = Number(document.getElementById("sales").value);
+
+let country = document.getElementById("country").value;
+
+let baseCost = price * qty;
+
+let taxRate = getCountryTax(country);
+
+let taxCost = baseCost * taxRate / 100;
+
+let landingCost = baseCost + freight + insurance + taxCost;
+
+let soldUnits = qty * sales / 100;
+
+let revenue = soldUnits * sell;
+
+let profit = revenue - landingCost;
+
+let pop = (profit / landingCost) * 100;
+
+let tds = (pop + sales) / 2;
+
+
+
+document.getElementById("landing").innerHTML =
+"Landing Cost: $" + landingCost.toFixed(2);
+
+document.getElementById("profit").innerHTML =
+"Profit: $" + profit.toFixed(2);
+
+document.getElementById("pop").innerHTML =
+"POP Score: " + pop.toFixed(2) + "%";
+
+document.getElementById("tds").innerHTML =
+"TDS Score: " + tds.toFixed(2);
 
 }
